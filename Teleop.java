03ebb79d3b80f2b03e78c7f179a1fecd4cfa53e5
acloud1927 <161884376+acloud1927@users.noreply.github.com>
2026-02-04 15:33:02 -0600

    Enhance Teleop with shooting adjustments
    Adjusted shooting logic.

--- Teleop.java
+++ Teleop.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -39,12 +43,19 @@ public class Teleop extends LinearOpMode {
     private VisionPortal visionPortal;
     private AprilTagProcessor aprilTag;
     
-    private int tick = 0;
-    
     private double targetSpeed = 1400;
     private boolean shootinGooooood = false;
     private double shootingTolerance = 50;
     private boolean Shooting = false;
+    private boolean REY1 = false;
+    
+    private double turningPower = 0;
+    
+    ElapsedTime tick = new ElapsedTime();
+    boolean waitingToShoot = false;
+
+    
+  
 
     @Override
     public void runOpMode() {
@@ -57,7 +68,7 @@ public class Teleop extends LinearOpMode {
         leftLift = hardwareMap.get(DcMotor.class, "leftLift");
         rightLift = hardwareMap.get(DcMotor.class, "rightLift");
         blocker = hardwareMap.get(Servo.class, "blocker");
-
+        
         // Create the AprilTag processor
         aprilTag = new AprilTagProcessor.Builder().build();
 
@@ -78,13 +89,17 @@ public class Teleop extends LinearOpMode {
         setManualExposure(8);  // exposure ms
 
 
+        
         telemetry.addLine("Ready for start; initializing AprilTag detection...");
         telemetry.update();
 
+
         waitForStart();
 
+     
         leftLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
         shooter.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         shooter.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
@@ -109,6 +124,7 @@ public class Teleop extends LinearOpMode {
         rightLift.setDirection(DcMotor.Direction.REVERSE);
      
         if (opModeIsActive()) {
+            try {
             while (opModeIsActive()) {
                 DriveTrain();
                 Telemetry();
@@ -116,6 +132,10 @@ public class Teleop extends LinearOpMode {
                 Intake();
                 Lift();
                 Block();
+                IntakeAdjustment();
+            }
+            } finally {
+                
             }
         }
     }
@@ -129,6 +149,21 @@ public class Teleop extends LinearOpMode {
         // Set exposure time
         exposureControl.setExposure(exposureMS, TimeUnit.MILLISECONDS);
     }
+    
+    private void IntakeAdjustment() {
+        if (gamepad2.y && REY1 == false) {
+            Shooting = true;
+            intake.setVelocity(-1500);
+            sleep(70);
+            intake.setVelocity(0);
+            Shooting = false;
+            REY1 = true;
+        } else if (gamepad2.y) {
+            REY1 = true;
+        } else {
+            REY1 = false;
+        }
+    }
 
     private void Block() {
         if (gamepad2.b == true) {
@@ -137,26 +172,40 @@ public class Teleop extends LinearOpMode {
             blocker.setPosition(0.15);
         }
     }
+    
+    private boolean withinShootingRange() {
+        return (shooter.getVelocity() > targetSpeed - shootingTolerance) && (shooter.getVelocity() < targetSpeed + shootingTolerance);
+    }
 
     private void Shooter() {
         if (gamepad2.left_trigger > 0) {
-          
+
             shooter.setVelocity((2 - (shooter.getVelocity()/targetSpeed)) * targetSpeed);
-     
-            if ((shooter.getVelocity() > targetSpeed - shootingTolerance) && (shooter.getVelocity() < targetSpeed + shootingTolerance)) {
-                sleep(100);
-                if ((shooter.getVelocity() > targetSpeed - shootingTolerance) && (shooter.getVelocity() < targetSpeed + shootingTolerance)) {
-                    shootinGooooood = true;
-                } else {
-                    shootinGooooood = false;
+            
+            if (withinShootingRange()) {
+                if (!waitingToShoot) {
+                    tick.reset();
+                    waitingToShoot = true;
+                    blocker.setPosition(0);
+                }
+            
+                if (tick.milliseconds() >= (2400 - (20*shootingTolerance))) {
+                    if (withinShootingRange()) {
+                        shootinGooooood = true;
+                    } else {
+                        shootinGooooood = false;
+                    }
+                    waitingToShoot = false;
                 }
             } else {
+                waitingToShoot = false;
                 shootinGooooood = false;
             }
         } else if (gamepad2.left_bumper) {
             shooter.setVelocity(-1300);
         } else {
             shooter.setVelocity(0);
+            blocker.setPosition(0.15);
         }
         
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
@@ -164,15 +213,16 @@ public class Teleop extends LinearOpMode {
         if (currentDetections.size() == 1) {
             for (AprilTagDetection detection : currentDetections) {
                 if (detection.ftcPose != null) {
-                    targetSpeed = 6.7 * detection.ftcPose.y + 922;
-                    shootingTolerance = 130 - detection.ftcPose.y;
+                    targetSpeed = Math.min(Math.max(6.7 * detection.ftcPose.y + 922, 1380), 1650);//6.7x + 922
+                    shootingTolerance = 110 - detection.ftcPose.y/2;//130 - x
                 }
             }
         } 
+        
     }
 
     private void Intake() {
-        if (gamepad2.right_trigger > 0) {
+        if (gamepad2.right_trigger > 0 || shootinGooooood == true) {
             intake.setVelocity(2000);
         } else if (gamepad2.right_bumper) {
             intake.setVelocity(-2000);
@@ -182,11 +232,14 @@ public class Teleop extends LinearOpMode {
     }
 
     private void Lift() {
-        if (gamepad1.dpad_down) {
-            leftLift.setPower(0.93);
+        
+     
+        
+        if (gamepad1.y) {
+            leftLift.setPower(0.95);
             rightLift.setPower(1);
-        } else if (gamepad1.dpad_up) {
-            leftLift.setPower(-0.93);
+        } else if (gamepad1.y) {
+            leftLift.setPower(-0.95);
             rightLift.setPower(-1);
         } else {
             leftLift.setPower(0);
@@ -195,66 +248,52 @@ public class Teleop extends LinearOpMode {
     }
 
     private void DriveTrain() {
-        if (!gamepad1.dpad_left) {
+        if (!gamepad2.dpad_left) {
             float Strafe;
             float Turn;
             float forward;
             double denominator;
 
-            Turn = -gamepad1.right_stick_x;
-            Strafe = gamepad1.left_stick_x;
-            forward = gamepad1.left_stick_y;
+            Turn = -gamepad2.right_stick_x;
+            Strafe = gamepad2.left_stick_x;
+            forward = gamepad2.left_stick_y;
             forward = forward * 1f;
             denominator = JavaUtil.maxOfList(JavaUtil.createListWith(1, Turn + forward + Strafe));
             Back_left.setPower((forward + (Strafe - Turn)) * denominator);
             Front_left.setPower((forward - (Strafe + Turn)) * denominator);
             Back_right.setPower((forward - (Strafe - Turn)) * denominator);
             Front_right.setPower((forward + Strafe + Turn) * denominator);
+            
         } else {
+            
             List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         
             if (currentDetections.size() == 1) {
                 for (AprilTagDetection detection : currentDetections) {
                     if (detection.id == 20 && detection.ftcPose != null) {
-                        if (detection.ftcPose.z > 9) {
-                            Back_left.setPower(0.1);
-                            Front_left.setPower(0.1);
-                            Back_right.setPower(-0.1);
-                            Front_right.setPower(-0.1);
-                        } else if (detection.ftcPose.z < 5) {
-                            Back_left.setPower(-0.1);
-                            Front_left.setPower(-0.1);
-                            Back_right.setPower(0.1);
-                            Front_right.setPower(0.1);
-                        } else {
-                            Back_left.setPower(0);
-                            Front_left.setPower(0);
-                            Back_right.setPower(0);
-                            Front_right.setPower(0);
-                        }
+                        
+                        turningPower = (detection.ftcPose.z - 10) * 0.02;
+                        
+                        Back_left.setPower(turningPower);
+                        Front_left.setPower(turningPower);
+                        Back_right.setPower(-turningPower);
+                        Front_right.setPower(-turningPower);
+                        
                     } else if (detection.id == 24 && detection.ftcPose != null) {
-                        if (detection.ftcPose.z > -0) {
-                            Back_left.setPower(0.1);
-                            Front_left.setPower(0.1);
-                            Back_right.setPower(-0.1);
-                            Front_right.setPower(-0.1);
-                        } else if (detection.ftcPose.z < -3) {
-                            Back_left.setPower(-0.1);
-                            Front_left.setPower(-0.1);
-                            Back_right.setPower(0.1);
-                            Front_right.setPower(0.1);
-                        } else {
-                            Back_left.setPower(0);
-                            Front_left.setPower(0);
-                            Back_right.setPower(0);
-                            Front_right.setPower(0);
-                        }
+                        
+                        turningPower = (detection.ftcPose.z - 1) * 0.02;
+                        
+                        Back_left.setPower(turningPower);
+                        Front_left.setPower(turningPower);
+                        Back_right.setPower(-turningPower);
+                        Front_right.setPower(-turningPower);
+                        
                     } 
                 }
             } 
         }
     }
-
+    
     private void Telemetry() {
         telemetry.addData("BACK_LEFT:", Back_left.getPower());
         telemetry.addData("BACK_RIGHT:", Back_right.getPower());
@@ -262,7 +301,8 @@ public class Teleop extends LinearOpMode {
         telemetry.addData("FRONT_LEFT:", Front_left.getPower());
         telemetry.addData("Shooting Power", shooter.getVelocity());
         telemetry.addData("Target Power", targetSpeed);
-        
+        telemetry.addData("Waiting Time", 2400 - (20*shootingTolerance));
+
             List<AprilTagDetection> currentDetections = aprilTag.getDetections();
 
             telemetry.addData("# Detected", currentDetections.size());
@@ -272,24 +312,33 @@ public class Teleop extends LinearOpMode {
                     if (detection.ftcPose != null) {
                         telemetry.addLine(String.format("ID: %d", detection.id));
                         telemetry.addLine(String.format("Pose: X=%.1f Y=%.1f Z=%.1f", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                        if (shootinGooooood == false) {
+                            telemetry.addData("", "\n" +
+                                "------------------------\n" +
+                                "|                            |\n" +
+                                "|                            |\n" +
+                                "|                            |\n" +
+                                "|                            |\n" +
+                                "|                            |\n" +
+                                "------------------------\n");
+                        } else {
+                            telemetry.addData("", "\n" +
+                                "------------------------\n" +
+                                "|                   /       |\n" +
+                                "|                 /         |\n" +
+                                "|    l          /           |\n" +
+                                "|       l     /             |\n" +
+                                "|          v                |\n" +
+                                "------------------------\n");
+                        }
                     }
                 }
-            } 
+            } else {
+                telemetry.addData(""," ");
+            }
+     
         
-        telemetry.addData("Tick", tick);
-        
-        if (shootinGooooood == true) {
-            telemetry.addData("", "\n" +
-        "                       /           \n" +
-        "                     /             \n" +
-        "                   /               \n" +
-        "                 /                 \n" +
-        "     l         /                   \n" +
-        "       l     /                     \n" +
-        "          v                        \n");
-        
-        }
-        
+      
         telemetry.update();
         
     }
